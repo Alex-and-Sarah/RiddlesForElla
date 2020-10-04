cccf48e9380f437c673fc1895d1a7b48c973d7ab
Sarah Smolen <ssmolen@pmc.com>
2020-10-04 11:51:56 -0400

    Initialize React app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles/app.css";
+import { introText } from "./riddles";
+
+function App() {
+  return <div className="App">{introText}</div>;
+}
+
+export default App;
