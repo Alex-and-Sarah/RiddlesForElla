a0fbfcce826ac58eb7ea9eb58523cc6db31bc5ea
Alex Morales <tamorales96@gmail.com>
2020-10-04 12:25:32 -0400

    Revert "Initialize React app"

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./styles/app.css";
-import { introText } from "./riddles";
-
-function App() {
-  return <div className="App">{introText}</div>;
-}
-
-export default App;
