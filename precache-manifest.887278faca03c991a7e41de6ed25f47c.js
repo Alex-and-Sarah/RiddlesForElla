d3dda1183a4d17fb3a5d6e149b9818b8b5b3e678
Sarah Smolen <ssmolen@pmc.com>
2020-10-05 11:51:41 -0400

    Updates

---
+++ precache-manifest.887278faca03c991a7e41de6ed25f47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7a741180e0c9e967e147d1fe9647a9",
+    "url": "/RiddlesForElla/index.html"
+  },
+  {
+    "revision": "9ef59dad17b56613151f",
+    "url": "/RiddlesForElla/static/css/main.3a256a23.chunk.css"
+  },
+  {
+    "revision": "df0f203a928e0aeff1b1",
+    "url": "/RiddlesForElla/static/js/2.8c998990.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RiddlesForElla/static/js/2.8c998990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef59dad17b56613151f",
+    "url": "/RiddlesForElla/static/js/main.0dfa4146.chunk.js"
+  },
+  {
+    "revision": "3dc0d9176152f9c709e6",
+    "url": "/RiddlesForElla/static/js/runtime-main.060eb89b.js"
+  }
+]);
